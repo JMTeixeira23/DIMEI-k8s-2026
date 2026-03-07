852c2cf098bd30e99aa0c12e0baace6e97d922e1
JM-Teixeira <joaoteixeira1923@gmail.com>
2026-03-07 13:06:46 +0000

    feat: upgrade EKS to 1.34

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -19,7 +19,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version for the EKS cluster"
   type        = string
-  default     = "1.29"
+  default     = "1.34"
 }
 
 variable "node_instance_type" {
